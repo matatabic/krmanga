afc78918506270989279db6223a926d6c82385c3
y <matatabi994@gmail.com>
2020-08-04 16:00:20 +0800

    edit TopBar

--- src/pages/views/TopTabBarWrapper.tsx
+++ src/pages/views/TopTabBarWrapper.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { MaterialTopTabBar, MaterialTopTabBarProps } from "@react-navigation/material-top-tabs";
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import Touchable from "@/components/Touchable";
 
 interface IProps extends MaterialTopTabBarProps {
 
@@ -11,7 +12,20 @@ class TopTabBarWrapper extends React.Component<IProps> {
   render() {
     return (
       <View style={styles.container}>
-        <MaterialTopTabBar {...this.props} />
+        <View style={styles.topTabBarView}>
+          <MaterialTopTabBar {...this.props} style={styles.tabBar} />
+          <Touchable style={styles.categoryBtn}>
+            <Text>分类</Text>
+          </Touchable>
+        </View>
+        <View style={styles.bottomTabBarView}>
+          <Touchable style={styles.searchBtn}>
+            <Text>Search</Text>
+          </Touchable>
+          <Touchable style={styles.historyBtn}>
+            <Text>History</Text>
+          </Touchable>
+        </View>
       </View>
     )
   }
@@ -19,7 +33,40 @@ class TopTabBarWrapper extends React.Component<IProps> {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#fff',
     paddingTop: getStatusBarHeight(),
+  },
+  topTabBarView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  tabBar: {
+    flex: 1,
+    elevation: 0,
+    overflow:'hidden',
+    backgroundColor:'transparent',
+  },
+  categoryBtn: {
+    paddingHorizontal: 10,
+    borderLeftWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ccc',
+  },
+  bottomTabBarView: {
+    flexDirection:'row',
+    paddingVertical:7,
+    paddingHorizontal:15,
+    alignItems:'center',
+  },
+  searchBtn: {
+    flex:1,
+    justifyContent:'center',
+    paddingLeft:12,
+    height:30,
+    borderRadius:15,
+    backgroundColor:'rgba(0,0,0,0.1)',
+  },
+  historyBtn: {
+    marginLeft:24,
   }
 })
 
